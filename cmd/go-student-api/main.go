f9b34eb14e7836e746a0cdb462660900965b02be
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-07-05 16:30:00 +0530

    Implement server setup and routing for the Go Student API

--- cmd/go-student-api/main.go
+++ cmd/go-student-api/main.go
@@ -1,8 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Sandeep-singh-99/go-student-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to the Go Student API!")
-	fmt.Println("This is a simple API to manage student records.")
+	// load the configuration
+	config := config.MustLoad()
+
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to the Go Student API!"))
+
+	})
+
+	// setup server
+
+	server := http.Server{
+		Addr:    config.Address,
+		Handler: router,
+	}
+
+	fmt.Println("Server is running on", config.Address)
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatalf("Error starting server: %v", err)
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 *time.Second)
+	defer cancel()
+
+	// err := server.Shutdown(ctx)
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Error shutting down server", "error", err)
+	} else {
+		slog.Info("Server shut down gracefully")
+	}
 }
